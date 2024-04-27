eefa8ef7ce1c34672155def5caffbaf0e791d1e5
AndrewWentzel <awentze2@uic.edu>
2024-04-27 12:21:43 -0500

    fixed a name

--- docker/python/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "Running digital twin notebook init"
-cd /workspace/DigitalTwinVis/python
-python TrainAllModels.py
--- frontend/src/modules/Endpoint.ts
+++ frontend/src/modules/Endpoint.ts
@@ -1,2 +1,3 @@
-
+//example for deplayment api
+// export const API_URL: string = 'https://servername.evl.uic.edu/api/'
 export const API_URL: string = 'http://127.0.0.1:5000/';//location of the backend api
